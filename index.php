8c815305641e9be0ed8570d938a5f9fb1b716eae
kirineko <kirinekoneko@gmail.com>
2018-04-04 03:14:06 +0800

    add weixin auth

--- index.php
+++ index.php
@@ -8,16 +8,43 @@
  */
 
 use Phalcon\Mvc\Micro;
+use Phalcon\Logger;
+use Phalcon\Logger\Adapter\File as FileAdapter;
+
+$logger = new FileAdapter('./logs/test.log');
 
 $app = new Micro();
 
 $app->get(
     "/wx",
-    function() use ($app) {
-        $app->response->setContentType("text/plain");
-        $app->response->sendHeaders();
-        $app->response->setContent("hello world!");
-        $app->response->send();
+    function() use ($app, $logger) {
+        try {
+            $request = $app->request;
+            $signature = $request->getQuery('signature');
+            $timestamp = $request->getQuery('timestamp');
+            $nonce = $request->getQuery('nonce');
+            $echostr = $request->getQuery('echostr');
+            $token = 'hello2016';
+
+            $list = [$token, $timestamp, $nonce];
+            sort($list);
+            $str_list = '';
+            foreach($list as $item) {
+                $str_list .= $item;
+            }
+            $hashcode = sha1($str_list);
+            $logger->info("handle/GET func: hashcode, signature: " . $hashcode . " " . $signature);
+            if ($hashcode == $signature) {
+                $app->response->setContent($echostr);
+                $app->response->send();
+            } else {
+                $app->response->setContent('');
+                $app->response->send();
+            }
+        } catch (Exception $e) {
+            $app->response->setContent($e);
+            $app->response->send();
+        }
     }
 );
 
